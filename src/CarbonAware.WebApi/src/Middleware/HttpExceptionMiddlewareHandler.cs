d588b9cf3a78c96d185f90dc90860e0d688b024b
Juan Zuluaga <juzuluag@microsoft.com>
2022-08-24 16:02:37 -0400

    Add problem details create method

--- src/CarbonAware.WebApi/src/Middleware/HttpExceptionMiddlewareHandler.cs
+++ src/CarbonAware.WebApi/src/Middleware/HttpExceptionMiddlewareHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Diagnostics;
 using System.Net;
 using System.Text.Json;
+using CarbonAware.Interfaces;
 using Microsoft.Extensions.Options;
 
 namespace CarbonAware.WebApi.Middleware;
@@ -47,14 +48,59 @@ public class HttpExceptionMiddlewareHandler
 
     private async Task HandleExceptionAsync(HttpContext context, Exception exception, MemoryStream buffer, Stream stream)
     {
-        var response = new HttpValidationProblemDetails()
-        {
-            Title = exception.GetType().ToString(),
-            Status = (int)HttpStatusCode.InternalServerError,
-            Detail = exception.Message
-        };
+        var response = CreateProblemDetailsResponse(context, exception);
+        context.Response.Clear();
+        context.Response.ContentType = "application/problem+json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        var json = JsonSerializer.Serialize<HttpValidationProblemDetails>(response);
+        await context.Response.WriteAsync(json);
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+        await buffer.CopyToAsync(stream);
+        context.Response.Body = stream;
+    }
 
-        var traceId = Activity.Current?.Id ?? context.TraceIdentifier;
+    private HttpValidationProblemDetails CreateProblemDetailsResponse(HttpContext context, Exception exception)
+    {
+        var activity = Activity.Current;
+        HttpValidationProblemDetails response;
+        if (exception is IHttpResponseException httpResponseException)
+        {
+            response = new HttpValidationProblemDetails(){
+                Title = httpResponseException.Title,
+                Status = httpResponseException.Status,
+                Detail = httpResponseException.Detail
+            };
+        } else {
+            var exceptionType = exception.GetType().Name;
+            int statusCode;
+            if (!EXCEPTION_STATUS_CODE_MAP.TryGetValue(exceptionType, out statusCode))
+            {
+                statusCode = (int)HttpStatusCode.InternalServerError;
+                activity?.SetStatus(ActivityStatusCode.Error, exception.Message);
+            }
+            var isVerboseApi = _options.CurrentValue.VerboseApi;
+       
+            if (statusCode == (int)HttpStatusCode.InternalServerError && !isVerboseApi)
+            {
+                 response = new HttpValidationProblemDetails() {
+                                Title = HttpStatusCode.InternalServerError.ToString(),
+                                Status = statusCode,
+                    };
+            }
+            else
+            {
+                response = new HttpValidationProblemDetails() {
+                            Title = exceptionType,
+                            Status = statusCode,
+                            Detail = exception.Message
+                };
+                if (isVerboseApi) {
+                    response.Errors["stackTrace"] = new string[] { exception.StackTrace! };
+                }
+            }
+        }
+
+        var traceId = activity?.Id;
         if (traceId != null)
         {
             response.Extensions["traceId"] = traceId;
@@ -66,13 +112,6 @@ public class HttpExceptionMiddlewareHandler
                 response.Errors[key] = messages;
             }
         }
-        context.Response.Clear();
-        context.Response.ContentType = "application/problem+json";
-        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        var json = JsonSerializer.Serialize<HttpValidationProblemDetails>(response);
-        await context.Response.WriteAsync(json);
-        context.Response.Body.Seek(0, SeekOrigin.Begin);
-        await buffer.CopyToAsync(stream);
-        context.Response.Body = stream;
+        return response;
     }
 }
