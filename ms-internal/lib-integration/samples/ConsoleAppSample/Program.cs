efc7fa246525f554d8575272e60f4dd5948228ac
Juan Zuluaga <juzuluag@microsoft.com>
2022-10-14 16:40:30 -0400

    Add Console App

---
+++ ms-internal/lib-integration/samples/ConsoleAppSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using GSF.CarbonIntensity.Handlers;
+using GSF.CarbonIntensity.Parameters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, ConsoleAppSample!");
+
+var key1 = $"{CarbonAwareVariablesConfiguration.Key}:CarbonIntensityDataSource";
+var settings = new Dictionary<string, string> {
+            {key1, "JSON"}
+        };
+var configuration = new ConfigurationBuilder()
+        .AddInMemoryCollection(settings)
+        .Build();
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddCarbonIntensityServices(configuration);
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var handler = serviceProvider.GetRequiredService<IEmissionsHandler>();
+
+const string startDate = "2022-03-01T15:30:00Z";
+const string endDate = "2022-03-01T18:30:00Z";
+const string location = "eastus";
+var builder = new EmissionsParametersBuilder();
+var param = builder.AddLocations(new string[] { location } )
+                .AddStartTime(DateTimeOffset.Parse(startDate))
+                .AddEndTime(DateTimeOffset.Parse(endDate))
+                .Build();
+
+var result = await handler.GetRatingAsync(param);
+Console.WriteLine($"For location {location} Starting at: {startDate} Ending at: {endDate} the Carbon Emissions Rating is: {result}.");
